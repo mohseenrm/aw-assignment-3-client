b034a919308d41befc05249c5c93f0765162c28b
mohseenrm <mohseenmukaddam6@gmail.com>
2017-10-22 16:08:08 -0700

    adding dev server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
+import * as elasticsearch from 'elasticsearch';
 
+const es = new elasticsearch.Client({
+  host: 'http://ec2-18-221-242-218.us-east-2.compute.amazonaws.com:9200',
+  log: 'trace',
+});
 export class App extends React.Component < any, any > {
   constructor (props: any) {
     super(props);
     this.state = {};
+  }
+
+  componentDidMount () {
+    console.log('Hola');
+    return es.search({
+      index: 'wiki',
+      type: 'items',
+      body: {
+        query: {
+          match: {
+            content: 'generics',
+          },
+        },
+      },
+    }).then((response) => {
+      console.log(response);
+    }).catch(err => console.log(err));
   }
 
   render () {

34e84f0fc0500e3bb24facb55de85522bafe5f8e
mohseenrm <mohseenmukaddam6@gmail.com>
2017-10-25 00:50:14 -0700

    updates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import * as elasticsearch from 'elasticsearch';
+import axios from 'axios';
 
 const es = new elasticsearch.Client({
   host: 'http://ec2-18-221-242-218.us-east-2.compute.amazonaws.com:9200',
   log: 'trace',
 });
+
+interface AppState {
+  url? : string;
+  lastUrl?: string;
+  response?: any;
+  responseTime: string;
+  appData? : any;
+}
 export class App extends React.Component < any, any > {
   constructor (props: any) {
     super(props);
     this.state = {};
   }
-
-  componentDidMount () {
+  // ES call example
+  /* componentDidMount () {
     console.log('Hola');
     return es.search({
       index: 'wiki',
@@ -26,13 +35,39 @@ export class App extends React.Component < any, any > {
     }).then((response) => {
       console.log(response);
     }).catch(err => console.log(err));
+  } */
+
+  componentDidMount () {
+    return axios({
+      method: 'get',
+      url: 'https://cdn.rawgit.com/mohseenrm/cdn/1e38b579/jsons/posts.json',
+    }).then((response: any) => {
+      console.log('App data: ', response.data);
+      const appData = {
+        appData: response.data,
+      };
+      this.setState(
+        Object.assign(
+          {},
+          this.state,
+          appData,
+        ),
+      );
+    });
   }
 
   render () {
+    if (this.state && this.state.appData) {
+      return(
+        <div className="main-wrapper">
+          Hola {this.props.message}!
+        </div>
+      );
+    }
     return(
-			<div className="main-wrapper">
-				Hola {this.props.message}!
-			</div>
+      <div className="main-wrapper">
+        Loading...
+      </div>
     );
   }
 }

5856f84a62347464ddb46be5c4d5af87bdadb3f9
mohseenrm <mohseenmukaddam6@gmail.com>
2017-10-26 02:22:38 -0700

    updates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,6 @@ import axios from 'axios';
 import { Posts } from './Posts';
 import { Response } from './Response';
 
-const es = new elasticsearch.Client({
-  host: 'http://ec2-18-221-242-218.us-east-2.compute.amazonaws.com:9200',
-  log: 'trace',
-});
-
 interface AppState {
   appData? : [{
     content: string;
@@ -27,28 +22,11 @@ export class App extends React.Component < any, AppState > {
 
     this.updateSelectedIndex = this.updateSelectedIndex.bind(this);
   }
-  // ES call example
-  /* componentDidMount () {
-    console.log('Hola');
-    return es.search({
-      index: 'wiki',
-      type: 'items',
-      body: {
-        query: {
-          match: {
-            content: 'generics',
-          },
-        },
-      },
-    }).then((response) => {
-      console.log(response);
-    }).catch(err => console.log(err));
-  } */
 
   componentDidMount () {
     return axios({
       method: 'get',
-      url: 'https://cdn.rawgit.com/mohseenrm/cdn/1e38b579/jsons/posts.json',
+      url: 'https://cdn.rawgit.com/mohseenrm/cdn/f4f138aa/jsons/posts.json',
     }).then((response: any) => {
       console.log('App data: ', response.data);
 
--- src/components/Response.tsx
+++ src/components/Response.tsx
@@ -1,20 +1,91 @@
 import * as React from 'react';
+import * as elasticsearch from 'elasticsearch';
+import { List, Segment } from 'semantic-ui-react';
 
-export class Response extends React.Component < ResponseProps, any > {
+const es = new elasticsearch.Client({
+  host: 'http://ec2-18-221-242-218.us-east-2.compute.amazonaws.com:9200',
+  log: 'trace',
+});
+export class Response extends React.Component < ResponseProps, ResponseState > {
   constructor (props: any) {
     super(props);
     this.state = {};
   }
 
+  componentWillReceiveProps (props: ResponseProps) {
+    console.log('Response props: ', props);
+    return es.search({
+      index: 'wiki',
+      type: 'items',
+      body: {
+        query: {
+          multi_match: {
+            query: this.generateQueryString(props.tags),
+            fields: [
+              'title',
+              'content',
+            ],
+          },
+        },
+      },
+    }).then((response) => {
+      const esResults = response.hits.hits.map(result => result._source);
+      console.log(esResults);
+      this.setState(
+				Object.assign(
+					{},
+					this.state,
+					{ esResults },
+				),
+			);
+    }).catch(err => console.log(err));
+  }
+
+  generateQueryString (tags: string[]) {
+    return tags.reduce((acc: string, current: string) => {
+			// tslint:disable-next-line
+      acc += ` OR \"${current}\"`;
+      return acc;
+    });
+  }
+
   render () {
+    if (this.state && this.state.esResults) {
+      console.log('TEST: ', this.generateQueryString(this.props.tags));
+      return(
+				<div className="main-wrapper--response">
+					<Segment inverted={true}>
+						<List divided={true} inverted={true} relaxed={true}>
+							{
+								this.state.esResults.map(result =>
+									<List.Item>
+										<List.Content>
+											<List.Header>{result.title.replace('[edit]', '')}</List.Header>
+											{result.content.slice(0, 50)}...
+										</List.Content>
+									</List.Item>,
+								)
+							}
+						</List>
+					</Segment>
+				</div>
+      );
+    }
     return(
-      <div className="main-wrapper--response">
-				Response Panel
-      </div>
+			<div className="main-wrapper--response">
+				Click on post title to load results..
+			</div>
     );
   }
 }
 
 interface ResponseProps {
   tags: string[];
+}
+
+interface ResponseState {
+  esResults?: [{
+    content: string;
+    title: string;
+  }];
 }
